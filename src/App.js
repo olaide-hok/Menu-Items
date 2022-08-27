9e5062a9bade6fecd15ed63d9aaf3c7a9737a9a2
olaide-hok <olaidekareem43@yahoo.com>
2022-08-27 10:25:24 +0100

    Complete Project

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map((item) => item.category)) ]
+
 function App() {
   const [menuItems, setMenuItems] = useState(items)
-  const [categories, setCategories] = useState([])
+  const [categories] = useState(allCategories)
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items)
+      return
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
 
   return (
     <main>
@@ -15,7 +26,7 @@ function App() {
           <div className="underline"></div>
         </div>
         
-        <Categories />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems}/>
       
       </section>
